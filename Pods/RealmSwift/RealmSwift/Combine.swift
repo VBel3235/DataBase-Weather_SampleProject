f84f93ae3aec710654fefac89e186d4199671445
VBel3235 <Vlad95.0753@gmain.com>
2021-07-14 07:43:34 +1000

    Fixed a little bug with realm

--- Pods/RealmSwift/RealmSwift/Combine.swift
+++ Pods/RealmSwift/RealmSwift/Combine.swift
@@ -35,6 +35,7 @@ import Realm.Private
 ///
 /// You can also manually conform to `Identifiable` if you wish, but note that
 /// using the object's memory address does *not* work for managed objects.
+@available(iOSApplicationExtension 13, *)
 public protocol ObjectKeyIdentifiable: Identifiable, Object {
     /// The stable identity of the entity associated with `self`.
     var id: UInt64 { get }
@@ -42,8 +43,9 @@ public protocol ObjectKeyIdentifiable: Identifiable, Object {
 
 /// :nodoc:
 @available(*, deprecated, renamed: "ObjectKeyIdentifiable")
-public typealias ObjectKeyIdentifable = ObjectKeyIdentifiable
+//public typealias ObjectKeyIdentifable = ObjectKeyIdentifiable
 
+@available(iOSApplicationExtension 13, *)
 extension ObjectKeyIdentifiable {
     /// A stable identifier for this object. For managed Realm objects, this
     /// value will be the same for all object instances which refer to the same
